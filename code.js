6f98631862e564ba838cdcad835b8c3778e7ce7a
nospec <stephanemathieu@hotmail.com>
2019-11-28 21:11:13 +1100

    working with js, added some style

---
+++ code.js
@@ -0,0 +1,32 @@
+/* javascript lets go */
+
+function myFunction(){
+    document.getElementById("demo").innerHTML = 'hi';
+    window.alert("the hell you doin here")
+}
+
+function myFunction1(){
+    window.alert("key pressed");
+}
+
+var x, y, z; // declare variables
+
+x = 5; // assign variabls
+y = 6;
+z = x + y; // compute values
+
+document.getElementById("demo").innerHTML = "I'm sad";
+
+a = 5; b = 6; c = a + b;
+
+// literals
+// numbers with and without decimals
+// 10.50 ... 1001
+// strings, text written with quotes
+h ="hello"; s ='hi';
+
+// variables
+var d;
+t = 8; 
+
+var object = {firstName: "stephane", lastName: "mathieu"} // object data type
